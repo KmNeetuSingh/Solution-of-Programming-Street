5eb5284db0a0c28612862891a59466651523b92d
Neetu Singh <neetuisngh99590@gmail.com>
2024-09-27 16:04:57 +0530

    armStrong

--- Sprint-1/17_Checking_for_Armstrong_Numbers_in_a_Range/IdealSolution/Armstrong.py
+++ Sprint-1/17_Checking_for_Armstrong_Numbers_in_a_Range/IdealSolution/Armstrong.py
@@ -3,7 +3,7 @@ arm = []
 for num in range(i[0], i[1] + 1):
     s = str(num)
     power = len(s)  
-    total_sum = sum(int(digit) ** power for digit in s)  # we are using hte built in function sum() ot add up all the value
+    total_sum = sum(int(digit) ** power for digit in s)  # we are using the built in function sum() ot add up all the value
     if total_sum == num:
         arm.append(num)  
 print(arm)
